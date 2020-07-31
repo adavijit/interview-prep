9eb2dea27412cd73a2e44322b24d3fc463891d43
Avijit Das <adavijit@yahoo.com>
2020-08-01 01:12:28 +0530

    Update InterviewPrepApplication.java

--- src/main/java/com/supriyanta/interviewprep/InterviewPrepApplication.java
+++ src/main/java/com/supriyanta/interviewprep/InterviewPrepApplication.java
@@ -9,5 +9,9 @@ public class InterviewPrepApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InterviewPrepApplication.class, args);
+        System.out.print("Bhai ekta call kor!!!");
+        
+        System.out.print("Thikachis tui????!!!");
+        
     }
 }
